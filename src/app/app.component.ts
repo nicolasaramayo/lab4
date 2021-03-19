4571c67c166f9c423d61d59bb78759cfd98012ab
nicolas <HIK_09@hotmail.com>
2021-03-18 23:06:16 -0300

    Primer commit. Modificaciones para Heroku

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mi-proyecto';
+
+  edaduno : number = 0;
+  edaddos : number = 0;
+  resultadoSuma : number = 0;
+  resultadoPromedio : number = 0;
+
+  sumar()
+  {
+    this.resultadoSuma = this.edaddos + this.edaduno;
+  }
+
+  promedio()
+  {
+    this.resultadoPromedio = (this.edaduno + this.edaddos) / 2;
+  }
+  
+  limpiar()
+  {
+    this.edaddos = 0;
+    this.edaduno = 0;
+    this.resultadoPromedio = 0;
+    this.resultadoSuma = 0;
+  }
+
+
+
 }
---
+++ src/entidades/usuario.ts
@@ -0,0 +1,10 @@
+export class Usuario {
+    clave:string;
+    nombre:string;
+    
+    public constructor(nombre:string,clave:string)
+    {
+        this.clave=clave;
+        this.nombre=nombre;
+    }
+}
